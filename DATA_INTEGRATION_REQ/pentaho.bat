050ba4debcd8a7d037e0d1ad82d73531c312a957
MiltonTwi <milton.quinteroes@campusucc.edu.co>
2024-11-15 20:41:56 -0500

    Data integration requisitos

---
+++ DATA_INTEGRATION_REQ/pentaho.bat
@@ -0,0 +1,3 @@
+
+"C:\Users\milton\Documents\Apps\data-integration\Kitchen.bat" /file="\\wsl.localhost\Ubuntu-22.04\home\config\Proyecto-Dockerizado-con-ETL-Oracle-y-PostgreSQL\DATA_INTEGRATION_REQ\PENTAHO\Job 1.kjb"
+
